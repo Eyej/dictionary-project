7f6b4bd4e4e06a5fcaed7e74a1990bb2dd576490
Eyej <Eyej@users.noreply.github.com>
2021-05-14 18:08:29 +0100

    Added new component, WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import Dictionary from "./Dictionary";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello Ij, Welcome to the dictionary project </h2>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          {" "}
+          <small>
+            Built by Ijeoma Onyenokporo, open source code on{" "}
+            <a href="https://github.com/Eyej/dictionary-project">Github</a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
