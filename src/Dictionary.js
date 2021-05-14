471a81318cce85e9f6c5bc2b290d873a62b40855
Eyej <Eyej@users.noreply.github.com>
2021-05-14 21:20:21 +0100

    completed form search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState(" ");
+
+  function handleSubmit(event) {
+    event.preventDefault(event);
+    alert(`Searching for the definition of ${keyword}`);
+  }
+
+  function getKeyword(event) {
+    setKeyword(event.target.value);
+  }
   return (
     <div className="Dictionary">
-      <p>First Search Item</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter any word"
+          onChange={getKeyword}
+        />
+      </form>
     </div>
   );
 }
